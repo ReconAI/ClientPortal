dc7e594a6012d437ae2671f42c9c3496281dddb6
Andrew <valasiuk.andrei@gmail.com>
2020-07-14 12:22:10 +0300

    RAI-102: US 4.2.2 Manufacturer CRUD Backend

--- client_portal/order_portal/serizalizers.py
+++ client_portal/order_portal/serizalizers.py
@@ -1,9 +1,63 @@
+from typing import List
+
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework.fields import ListField
 from rest_framework.serializers import ModelSerializer
 
-from recon_db_manager.models import Category
+from recon_db_manager.models import Category, Manufacturer
 
 
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
-        fields = ('name',)
+        fields = ('id', 'name',)
+
+
+class ReadManufacturerSerializer(ModelSerializer):
+    categories = CategorySerializer(many=True, allow_null=True)
+    category_ids = serializers.ListSerializer
+
+    class Meta:
+        model = Manufacturer
+        fields = ('name', 'categories')
+
+
+class WriteManufacturerSerializer(ModelSerializer):
+    category_ids = ListField(
+        child=serializers.IntegerField(min_value=1),
+        allow_empty=True,
+        min_length=0,
+        max_length=None
+    )
+
+    class Meta:
+        model = Manufacturer
+        fields = ('name', 'category_ids')
+
+    @staticmethod
+    def validate_category_ids(category_ids: List[int]):
+        category_ids = set(category_ids)
+
+        categories = Category.objects.filter(id__in=category_ids).all()
+
+        if category_ids and len(categories) != len(category_ids):
+            raise ValidationError("Some of passed categories does not exist")
+
+        return categories
+
+    def create(self, validated_data):
+        categories = validated_data.pop('category_ids')
+
+        manufacturer = super().create(validated_data)
+        manufacturer.categories.set(categories)
+
+        return manufacturer
+
+    def update(self, instance, validated_data):
+        categories = validated_data.pop('category_ids')
+
+        manufacturer = super().update(instance, validated_data)
+        manufacturer.categories.set(categories)
+
+        return manufacturer
