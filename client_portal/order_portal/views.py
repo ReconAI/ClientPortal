1aa0ac0534818a7f6e43060108dd067a6f38832a
Andrew <valasiuk.andrei@gmail.com>
2020-07-15 13:10:55 +0300

    RAI-45: non-paginated category list

--- client_portal/order_portal/views.py
+++ client_portal/order_portal/views.py
@@ -11,7 +11,8 @@ from order_portal.serizalizers import CategorySerializer, \
 from recon_db_manager.models import Category
 from shared.permissions import IsActive, IsSuperUser, PaymentRequired
 from shared.swagger.headers import token_header
-from shared.swagger.responses import get_responses
+from shared.swagger.responses import get_responses, data_serializer, http400, \
+    http401, http405, http404, http403
 
 
 class CategoryOperator:
@@ -40,6 +41,21 @@ class CategoryOperator:
         token_header(),
     ]
 ))
+@method_decorator(name='get', decorator=swagger_auto_schema(
+    responses={
+        status.HTTP_200_OK: data_serializer(CategorySerializer),
+        status.HTTP_400_BAD_REQUEST: http400(),
+        status.HTTP_401_UNAUTHORIZED: http401(),
+        status.HTTP_403_FORBIDDEN: http403(),
+        status.HTTP_404_NOT_FOUND: http404(),
+        status.HTTP_405_METHOD_NOT_ALLOWED: http405()
+    },
+    operation_summary="Categories retrieval",
+    operation_description='Obtains list of categories',
+    manual_parameters=[
+        token_header(),
+    ]
+))
 class CategoryList(CategoryOperator, ListCreateAPIView):
     def create(self, request, *args, **kwargs):
         serializer = CategoryCollectionSerializer(data=request.data)
@@ -52,6 +68,13 @@ class CategoryList(CategoryOperator, ListCreateAPIView):
             }, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        return Response({
+            'data': self.get_serializer(queryset, many=True).data
+        })
 
 
 class CategoryItem(CategoryOperator, RetrieveUpdateDestroyAPIView):
