b9981dcaac1ed020b8a2595d2c1ec73568e51487
Andrew <valasiuk.andrei@gmail.com>
2020-07-15 13:03:28 +0300

    RAI-45: categories as multiple field

--- client_portal/order_portal/serizalizers.py
+++ client_portal/order_portal/serizalizers.py
@@ -1,4 +1,7 @@
-from rest_framework.serializers import ModelSerializer
+from typing import List
+
+from rest_framework.serializers import ModelSerializer, ListSerializer, \
+    Serializer
 
 from recon_db_manager.models import Category
 
@@ -7,3 +10,15 @@ class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
         fields = ('name',)
+
+
+class CategoryCollectionSerializer(Serializer):
+    categories = ListSerializer(child=CategorySerializer(), allow_null=False,
+                                allow_empty=False)
+
+    def create(self, validated_data: List[dict]):
+        return [
+            Category.objects.create(**category_data)
+            for category_data
+            in validated_data.get('categories', [])
+        ]
--- client_portal/order_portal/views.py
+++ client_portal/order_portal/views.py
@@ -1,10 +1,17 @@
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, \
     RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from order_portal.serizalizers import CategorySerializer
+from order_portal.serizalizers import CategorySerializer, \
+    CategoryCollectionSerializer
 from recon_db_manager.models import Category
 from shared.permissions import IsActive, IsSuperUser, PaymentRequired
+from shared.swagger.headers import token_header
+from shared.swagger.responses import get_responses
 
 
 class CategoryOperator:
@@ -16,8 +23,35 @@ class CategoryOperator:
     queryset = Category.objects.all()
 
 
+@method_decorator(name='post', decorator=swagger_auto_schema(
+    responses=get_responses(
+        status.HTTP_200_OK,
+        status.HTTP_400_BAD_REQUEST,
+        status.HTTP_401_UNAUTHORIZED,
+        status.HTTP_404_NOT_FOUND,
+        status.HTTP_403_FORBIDDEN,
+        status.HTTP_405_METHOD_NOT_ALLOWED,
+        status.HTTP_422_UNPROCESSABLE_ENTITY
+    ),
+    request_body=CategoryCollectionSerializer,
+    operation_summary="Categories creation",
+    operation_description='Creates list of categories',
+    manual_parameters=[
+        token_header(),
+    ]
+))
 class CategoryList(CategoryOperator, ListCreateAPIView):
-    pass
+    def create(self, request, *args, **kwargs):
+        serializer = CategoryCollectionSerializer(data=request.data)
+
+        if serializer.is_valid():
+            self.perform_create(serializer)
+
+            return Response({
+                'message': 'Categories were added'
+            }, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
 class CategoryItem(CategoryOperator, RetrieveUpdateDestroyAPIView):
