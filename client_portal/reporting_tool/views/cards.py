b245c56a13d0d95f9e7620b7454b5876878380ce
Andrew <valasiuk.andrei@gmail.com>
2020-07-22 14:27:49 +0300

    RAI-116: detach cards

--- client_portal/reporting_tool/views/cards.py
+++ client_portal/reporting_tool/views/cards.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from reporting_tool.serializers import AddCardSerializer
+from reporting_tool.serializers import AttachPaymentMethodSerializer, DetachPaymentMethodSerializer
 from shared.permissions import IsCompanyDeveloper, IsActive
 from shared.swagger.headers import token_header
 from shared.swagger.responses import DEFAULT_UNSAFE_REQUEST_RESPONSES
@@ -14,16 +14,15 @@ class CardListView(ListCreateAPIView):
     permission_classes = (IsAuthenticated, IsActive,
                           IsCompanyDeveloper)
 
-    write_serializer_class = AddCardSerializer
+    serializer_class = AttachPaymentMethodSerializer
+
+    delete_serializer_class = DetachPaymentMethodSerializer
 
     def get(self, request: Request, *args, **kwargs) -> Response:
-        return Response({
-            'data': request.user.organization.customer.payment_methods().list('card')
-        })
+        return Response(request.user.organization.customer.payment_methods().list('card'))
 
     @swagger_auto_schema(
         responses=DEFAULT_UNSAFE_REQUEST_RESPONSES,
-        request_body=AddCardSerializer,
         tags=['Cards'],
         operation_summary="Attach a card",
         operation_description='Attaches created card to organization',
@@ -34,5 +33,22 @@ class CardListView(ListCreateAPIView):
     def post(self, request: Request, *args, **kwargs) -> Response:
         return self.save_or_error(
             'Card is attached',
-            self.write_serializer_class(instance=request.user.organization, data=request.data)
+            self.serializer_class(instance=request.user.organization, data=request.data)
+        )
+
+    @swagger_auto_schema(
+        responses=DEFAULT_UNSAFE_REQUEST_RESPONSES,
+        request_body=delete_serializer_class,
+        tags=['Cards'],
+        operation_summary="Detach a card",
+        operation_description='Detaches created card from organization',
+        manual_parameters=[
+            token_header(),
+        ]
+    )
+    def delete(self, request):
+        return self.save_or_error(
+            'Card is detached',
+            self.delete_serializer_class(instance=request.user.organization,
+                                  data=request.data)
         )
