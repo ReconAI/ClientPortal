e5cb7b5946f1d5e8a558583952ed123cf8ee46e5
Aliaksandr <a.adziareika@hqsoftwarelab.com>
2020-08-05 14:41:30 +0300

    RAI-47: add snackbar of max amount of files;

---
+++ recon_front/reconAi/src/app/reporting/components/reporting-list-devices/reporting-list-devices.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'recon-reporting-list-devices',
+  templateUrl: './reporting-list-devices.component.html',
+  styleUrls: ['./reporting-list-devices.component.less']
+})
+export class ReportingListDevicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- recon_front/reconAi/src/app/reporting/reporting-routing.module.ts
+++ recon_front/reconAi/src/app/reporting/reporting-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReportingListDevicesContainer } from './components/reporting-list-devices/reporting-list-devices.container';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ReportingListDevicesContainer,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ReportingRoutingModule { }
+export class ReportingRoutingModule {}
