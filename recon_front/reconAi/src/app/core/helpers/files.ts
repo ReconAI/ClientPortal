1445d4a00fde35d398f8407e41a76a86300c3279
Aliaksandr <a.adziareika@hqsoftwarelab.com>
2020-08-17 11:29:27 +0300

    RAI-96: add names of files;

--- recon_front/reconAi/src/app/core/helpers/files.ts
+++ recon_front/reconAi/src/app/core/helpers/files.ts
@@ -2,7 +2,10 @@ export function getBase64(file: File) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
+    reader.onload = () => {
+      const result = `name:${file.name};${reader.result}`;
+      return resolve(result);
+    };
     reader.onerror = (error) => reject(error);
   });
 }
