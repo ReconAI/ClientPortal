7badafb8029f666f28f5c12f48644a8bcd876ff9
Aliaksandr <a.adziareika@hqsoftwarelab.com>
2020-08-20 12:16:14 +0300

    RAI-161: finish filters list; fix bug related to map;

---
+++ recon_front/reconAi/src/app/shared/forms/filters/filter-two-inputs/filter-two-inputs.component.ts
@@ -0,0 +1,71 @@
+import { NgControl } from '@angular/forms';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  Optional,
+  Self,
+} from '@angular/core';
+
+export interface FilterTwoInputsInterface {
+  left: string;
+  right: string;
+}
+
+@Component({
+  selector: 'recon-filter-two-inputs',
+  templateUrl: './filter-two-inputs.component.html',
+  styleUrls: ['./filter-two-inputs.component.less'],
+})
+export class FilterTwoInputsComponent implements OnInit {
+  left = '';
+  right = '';
+  @Input() disabled = false;
+  @Output() changeVal = new EventEmitter<FilterTwoInputsInterface>();
+
+  @Input() leftPlaceholder = '';
+  @Input() rightPlaceholder = '';
+
+  @Input() label = '';
+
+  constructor(@Optional() @Self() public controlDir: NgControl) {
+    controlDir.valueAccessor = this;
+  }
+
+  onChange = (value: any) => {};
+  onTouched = () => {};
+
+  writeValue(value: FilterTwoInputsInterface) {
+    this.left = value.left;
+    this.right = value.right;
+
+    // this.onChange(this.checked);
+    // this.changeVal.emit(this.checked);
+  }
+
+  registerOnChange(fn) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn) {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  changeValue(): void {
+    const newValue = {
+      left: this.left,
+      right: this.right,
+    };
+
+    this.onChange(newValue);
+    this.changeVal.emit(newValue);
+  }
+
+  ngOnInit(): void {}
+}
